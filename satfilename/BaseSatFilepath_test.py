d2f0cc3bb0af3b8f412761e010681056152f46e3
Tylar Murray <murray.tylar@gmail.com>
2018-01-25 20:40:20 +0000

    also test for failure when expected

--- satfilename/BaseSatFilepath_test.py
+++ satfilename/BaseSatFilepath_test.py
@@ -19,3 +19,12 @@ class Test_BaseSatFilepath(TestCase):
         self.assertTrue(
             test_namer.match_filename("my_test2017-01-01T12:00:00Z.path")
         )
+    def test_match_filename_fail(self):
+        """
+        same as test_match_filename_success, but with slightly malformed
+        filename.
+        """
+        test_namer = BaseSatFilepath("test", "my_test%Y-%m-%dT%H:%M:%SZ.path")
+        self.assertFalse(
+            test_namer.match_filename("my_test2017-01-01TABC:00:00Z.path")
+        )
